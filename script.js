2865a547d395958efd5fd4f4096c2a80c059e178
Hashemi Rafsa Alfatih <155339599+hrafsa@users.noreply.github.com>
2024-06-22 18:13:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let dest = new Date("June 29, 2024 10:00:00").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let diff = dest - now;
+
+  // Check if the countdown has reached zero or negative
+  if (diff <= 0) {
+    clearInterval(x); // Stop the countdown
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+    return; // Exit the function
+  }
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  if (days < 10) {
+    days = `0${days}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  // Update the countdown timer on the page
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
